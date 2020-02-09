29cfe40dd2016fb89344c25f2503dabc51e34173
KOTRAK\bartlomiej.soter <bartlomiej.soter@kotrak.com>
2020-02-09 22:22:38 +0100

    #3 - Implemented enumerable extensions for sum of weights;

--- src/QuantitativeWorld/EnumerableExtensions.cs
+++ src/QuantitativeWorld/EnumerableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuantitativeWorld.DotNetExtensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,10 +7,17 @@ namespace QuantitativeWorld
 {
     public static class EnumerableExtensions
     {
-        public static Weight Sum(this IEnumerable<Weight> source) =>
-            throw new NotImplementedException();
+        public static Weight Sum(this IEnumerable<Weight> source)
+        {
+            Assert.IsNotNull(source, nameof(source));
+            return source.Aggregate(default(Weight), (acc, e) => acc + e);
+        }
 
-        public static Weight Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, Weight> selector) =>
-            throw new NotImplementedException();
+        public static Weight Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, Weight> selector)
+        {
+            Assert.IsNotNull(source, nameof(source));
+            Assert.IsNotNull(selector, nameof(selector));
+            return source.Aggregate(default(Weight), (acc, e) => acc + selector(e));
+        }
     }
 }
