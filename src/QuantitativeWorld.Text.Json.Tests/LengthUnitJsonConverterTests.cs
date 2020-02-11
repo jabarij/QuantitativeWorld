604ef9c3cc6b808a615cb8a8283473865583a127
KOTRAK\bartlomiej.soter <bartlomiej.soter@kotrak.com>
2020-02-11 20:09:38 +0100

    #5 - Developed unit tests for length unit JSON converter;

---
+++ src/QuantitativeWorld.Text.Json.Tests/LengthUnitJsonConverterTests.cs
@@ -0,0 +1,89 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Newtonsoft.Json;
+using QuantitativeWorld.TestAbstractions;
+using Xunit;
+
+namespace QuantitativeWorld.Text.Json.Tests
+{
+    public class LengthUnitJsonConverterTests : TestsBase
+    {
+        public LengthUnitJsonConverterTests(TestFixture testFixture) : base(testFixture) { }
+
+        [Theory]
+        [InlineData(LinearUnitJsonSerializationFormat.AlwaysFull, "{\"Unit\":{\"Name\":\"millimetre\",\"Abbreviation\":\"g\",\"ValueInMetres\":0.001}}")]
+        [InlineData(LinearUnitJsonSerializationFormat.PredefinedAsString, "{\"Unit\":\"mm\"}")]
+        public void SerializePredefinedUnit_ShouldReturnValidJson(LinearUnitJsonSerializationFormat serializationFormat, string expectedJson)
+        {
+            // arrange
+            var unit = LengthUnit.Millimetre;
+            var converter = new LengthUnitJsonConverter(serializationFormat);
+
+            // act
+            string actualJson = JsonConvert.SerializeObject(new SomeUnitOwner<LengthUnit> { Unit = unit }, converter);
+
+            // assert
+            actualJson.Should().Be(expectedJson);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestData), typeof(LengthUnit), nameof(LengthUnit.GetPredefinedUnits))]
+        public void DeserializePredefinedUnitString_ShouldReturnValidResult(LengthUnit expectedUnit)
+        {
+            // arrange
+            string json = $@"{{
+  'unit': '{expectedUnit.Abbreviation}'
+}}";
+            var converter = new LengthUnitJsonConverter();
+
+            // act
+            var result = JsonConvert.DeserializeObject<SomeUnitOwner<LengthUnit>>(json, converter);
+
+            // assert
+            result.Unit.Should().Be(expectedUnit);
+        }
+
+        [Fact]
+        public void DeserializeCustomUnit_ShouldReturnValidResult()
+        {
+            // arrange
+            string json = @"{
+  'name': 'some unit',
+  'abbreviation': 'su',
+  'valueInMetres': 123.456
+}";
+            var converter = new LengthUnitJsonConverter();
+
+            // act
+            var result = JsonConvert.DeserializeObject<LengthUnit>(json, converter);
+
+            // assert
+            result.Name.Should().Be("some unit");
+            result.Abbreviation.Should().Be("su");
+            result.ValueInMetres.Should().Be(123.456m);
+        }
+
+        [Theory]
+        [InlineData(LinearUnitJsonSerializationFormat.AlwaysFull)]
+        [InlineData(LinearUnitJsonSerializationFormat.PredefinedAsString)]
+        public void SerializeAndDeserialize_ShouldBeIdempotent(LinearUnitJsonSerializationFormat serializationFormat)
+        {
+            // arrange
+            var obj = new SomeUnitOwner<LengthUnit> { Unit = Fixture.Create<LengthUnit>() };
+            var converter = new LengthUnitJsonConverter(serializationFormat);
+
+            // act
+            string serializedObj1 = JsonConvert.SerializeObject(obj, converter);
+            var deserializedObj1 = JsonConvert.DeserializeObject<SomeUnitOwner<LengthUnit>>(serializedObj1, converter);
+            string serializedObj2 = JsonConvert.SerializeObject(deserializedObj1, converter);
+            var deserializedObj2 = JsonConvert.DeserializeObject<SomeUnitOwner<LengthUnit>>(serializedObj2, converter);
+
+            // assert
+            deserializedObj1.Unit.Should().Be(obj.Unit);
+            deserializedObj2.Unit.Should().Be(obj.Unit);
+
+            deserializedObj2.Unit.Should().Be(deserializedObj1.Unit);
+            serializedObj2.Should().Be(serializedObj1);
+        }
+    }
+}
