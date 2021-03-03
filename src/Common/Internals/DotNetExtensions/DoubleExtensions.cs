2e4a22e3b47a4411e4c20d145cea264707c88c1a
bartek.soter@gmail.com <10560885+jabarij@users.noreply.github.com>
2021-03-03 08:13:58 +0100

    - Created decimal projects; - Created decimal solution;

---
+++ src/Common/Internals/DotNetExtensions/DoubleExtensions.cs
@@ -0,0 +1,33 @@
+﻿#if DECIMAL
+namespace DecimalQuantitativeWorld.DotNetExtensions
+#else
+namespace QuantitativeWorld.DotNetExtensions
+#endif
+{
+    internal static class DoubleExtensions
+    {
+        public static double Pow(this double value, int exp)
+        {
+            double result;
+            if (exp == 0)
+                result = 1d;
+            else if (value == 0d || value == 1d)
+                result = value;
+            else if (exp > 0)
+            {
+                result = 1d;
+                for (int i = 0; i < exp; i++)
+                    result *= value;
+            }
+            else
+            {
+                result = 1d;
+                for (int i = 0; i > exp; i--)
+                    result *= value;
+                result = 1 / result;
+            }
+
+            return result;
+        }
+    }
+}
